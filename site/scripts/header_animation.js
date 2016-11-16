5c0261a9ecf791fe1d791e4612eaf3546a9097e7
talrez <tal@way2cu.com>
2016-11-16 11:08:25 +0200

    Added header_animation js file

---
+++ site/scripts/header_animation.js
@@ -0,0 +1,45 @@
+/**
+ * Header animation JavaScript
+ * XYZ Desings
+ *
+ * Copyright (c) 2015. by Way2CU, http://way2cu.com
+ * Authors: Tal Reznic
+ */
+
+// create or use existing site scope
+var Site = Site || {};
+
+Site.Animation = function(element, element_stop) {
+	var self = this;
+
+	self.element = document.querySelector(element);
+	self.element_stop_position = document.querySelector(element_stop).clientHeight;
+	self.start = 0;
+	self.rgb = {r: 35, g: 31, b: 32};
+
+	self._init = function() {
+
+		window.addEventListener('scroll', self.handle_scroll);
+	}
+
+	self.handle_scroll = function(event) {
+		var scroll = window.scrollY;
+		var alpha = null;
+
+		if(scroll > self.element_stop_position) {
+			alpha = 1;
+		} else if(scroll < self.element_stop_position) {
+			alpha = (scroll - self.start) / self.element_stop_position;
+		}
+
+		// set background color to element
+		self.element.style.backgroundColor = 'rgba('+self.rgb.r+', '+self.rgb.g+', '+self.rgb.b+', '+alpha+')';
+	}
+
+	// finish object initialization
+	self._init();
+}
+
+$(function() {
+	Site.animate_header = Site.Animation('header', 'div.slider');
+})
